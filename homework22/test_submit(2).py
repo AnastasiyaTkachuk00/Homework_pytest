0d9f97fa6b1fe0b920be7a59687ad3296a933540
Anastasiya Tkachuk <118803480+AnastasiyaTkachuk00@users.noreply.github.com>
2023-04-01 12:51:06 +0300

    Update test_submit(2).py

--- homework22/test_submit(2).py
+++ homework22/test_submit(2).py
@@ -8,16 +8,22 @@ link = "https://ultimateqa.com/filling-out-forms/"
 browser.get(link)
 browser.maximize_window()
 
-name_field = browser.find_element(By.ID, 'et_pb_contact_name_0')
-name_field.click()
-name_field.send_keys('Test')
-time.sleep(3)
 
-submit_button = browser.find_element(By.NAME, 'et_builder_submit_button')
-submit_button.click()
-time.sleep(8)
+def test_name_field():
+    name_field = browser.find_element(By.ID, 'et_pb_contact_name_0')
+    name_field.click()
+    name_field.send_keys('Test')
+    time.sleep(3)
 
-success_message = browser.find_element(By.XPATH, '//*[.="Please, fill in the following fields:"]')
-assert success_message == 'Please, fill in the following fields:'
 
-browser.close()
+def test_submit_button():
+    submit_button = browser.find_element(By.NAME, 'et_builder_submit_button')
+    submit_button.click()
+    time.sleep(8)
+
+
+def test_success_message():
+    success_message = browser.find_element(By.XPATH, '//*[.="Please, fill in the following fields:"]')
+    assert "Please, fill in the following fields:" in success_message
+
+    browser.close()

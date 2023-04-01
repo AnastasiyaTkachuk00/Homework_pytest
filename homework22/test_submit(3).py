9f264b44c817813f3202358126b2a8cff6ad9583
Anastasiya Tkachuk <118803480+AnastasiyaTkachuk00@users.noreply.github.com>
2023-04-01 12:52:38 +0300

    Update test_submit(3).py

--- homework22/test_submit(3).py
+++ homework22/test_submit(3).py
@@ -7,17 +7,23 @@ link = "https://ultimateqa.com/filling-out-forms/"
 browser.get(link)
 browser.maximize_window()
 
-message_field = browser.find_element(By.ID, 'et_pb_contact_message_0')
-message_field.click()
-message_field.send_keys('Test')
-time.sleep(4)
 
-submit_button = browser.find_element(By.NAME, 'et_builder_submit_button')
-submit_button.click()
-time.sleep(5)
+def test_message_field():
+    message_field = browser.find_element(By.ID, 'et_pb_contact_message_0')
+    message_field.click()
+    message_field.send_keys('Test')
+    time.sleep(4)
 
-success_message = browser.find_element(By.XPATH, '//*[@id="et_pb_contact_form_0"]/div/p')
-time.sleep(10)
-assert success_message == 'Please, fill in the following fields:'
 
-browser.close()
+def test_submit_button():
+    submit_button = browser.find_element(By.NAME, 'et_builder_submit_button')
+    submit_button.click()
+    time.sleep(5)
+
+
+def test_success_message():
+    success_message = browser.find_element(By.XPATH, '//*[@id="et_pb_contact_form_0"]/div/p')
+    time.sleep(10)
+    assert success_message == 'Please, fill in the following fields:'
+
+    browser.close()

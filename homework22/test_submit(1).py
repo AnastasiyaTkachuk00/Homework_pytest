5188c9cc8cf2adcd00cf6119a05d59a586e40aff
Anastasiya Tkachuk <118803480+AnastasiyaTkachuk00@users.noreply.github.com>
2023-04-01 12:47:59 +0300

    Update test_submit(1).py

--- homework22/test_submit(1).py
+++ homework22/test_submit(1).py
@@ -1,5 +1,4 @@
 import time
-import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -8,22 +7,29 @@ link = "https://ultimateqa.com/filling-out-forms/"
 browser.get(link)
 browser.maximize_window()
 
-name_field = browser.find_element(By.ID, 'et_pb_contact_name_0')
-name_field.click()
-name_field.send_keys('Test')
-time.sleep(3)
 
-message_field = browser.find_element(By.ID, 'et_pb_contact_message_0')
-message_field.click()
-message_field.send_keys('Test')
-time.sleep(4)
+def test_name_field():
+    name_field = browser.find_element(By.ID, 'et_pb_contact_name_0')
+    name_field.click()
+    name_field.send_keys('Test')
+    time.sleep(3)
 
-submit_button = browser.find_element(By.NAME, 'et_builder_submit_button')
-submit_button.click()
-time.sleep(5)
 
-success_message = browser.find_element(By.XPATH, '//*[@id="et_pb_contact_form_0"]/div/p')
-time.sleep(10)
-assert success_message == 'Thanks for contacting us'
+def test_message_field():
+    message_field = browser.find_element(By.ID, 'et_pb_contact_message_0')
+    message_field.click()
+    message_field.send_keys('Test')
+    time.sleep(4)
 
-browser.close()
+
+def test_submit_button():
+    submit_button = browser.find_element(By.NAME, 'et_builder_submit_button')
+    submit_button.click()
+    time.sleep(5)
+
+
+def test_success_message():
+    success_message = browser.find_element(By.CSS_SELECTOR, '#et_pb_contact_form_0 > div > p')
+    time.sleep(5)
+
+    assert 'Thanks for contacting us' in success_message

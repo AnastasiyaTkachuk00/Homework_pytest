82aa4aa7839631fab32ee210b905c3ce3d40672b
Anastasiya Tkachuk <118803480+AnastasiyaTkachuk00@users.noreply.github.com>
2023-04-01 12:06:51 +0300

    Update test_clicker.py

--- homework22/test_clicker.py
+++ homework22/test_clicker.py
@@ -5,20 +5,25 @@ browser = webdriver.Chrome()
 link = "https://ultimateqa.com/complicated-page/"
 browser.get(link)
 
-css_button = browser.find_element(By.CSS_SELECTOR, 'div.et_pb_button_module_wrapper'
-                                                   '.et_pb_button_4_wrapper'
-                                                   '.et_pb_button_alignment_left.et_pb_module  a')
-css_button.click()
 
-xpath_button = browser.find_element(By.XPATH, '//div[@class="et_pb_button_module_wrapper '
-                                              'et_pb_button_4_wrapper '
-                                              'et_pb_button_alignment_left et_pb_module"]/a')
-xpath_button.click()
+def test_button_by_css():
+    css_button = browser.find_element(By.CSS_SELECTOR, 'div.et_pb_button_module_wrapper'
+                                                       '.et_pb_button_4_wrapper'
+                                                       '.et_pb_button_alignment_left.et_pb_module  a')
+    css_button.click()
 
 
-class_button = browser.find_element(By.CLASS_NAME, 'et_pb_button_module_wrapper '
-                                                   'et_pb_button_4_wrapper '
-                                                   'et_pb_button_alignment_left et_pb_module')
-class_button.click()
+def test_button_by_xpath():
+    xpath_button = browser.find_element(By.XPATH, '//div[@class="et_pb_button_module_wrapper '
+                                                  'et_pb_button_4_wrapper '
+                                                  'et_pb_button_alignment_left et_pb_module"]/a')
+    xpath_button.click()
 
-browser.close()
+
+def test_button_by_classname():
+    class_button = browser.find_element(By.CLASS_NAME, "et_pb_button_module_wrapper "
+                                                       "et_pb_button_3_wrapper "
+                                                       "et_pb_button_alignment_left "
+                                                       "et_pb_module")
+    class_button.click()
+    browser.close()
